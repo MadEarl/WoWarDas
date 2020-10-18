5ed9ae21cba601b5ba7dc538f385e3fb1f8c35e5
Wolfgang Mederle <madearl@gmail.com>
2020-10-18 22:23:11 +0200

    DB queries.

--- app/src/main/java/de/mederle/wowardas/StorageSQL.kt
+++ app/src/main/java/de/mederle/wowardas/StorageSQL.kt
@@ -2,26 +2,42 @@ package de.mederle.wowardas
 
 import android.content.ContentValues
 import android.content.Context
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
 // wanted REAL for date, but values.put did not like it
 class Entry {
+    var id: Int? = null
     var latitude: Float
     var longitude: Float
     var time: Long
 
+    constructor(id: Int, latitude: Float, longitude: Float, time: Long) {
+        this.id = id
+        this.latitude = latitude
+        this.longitude = longitude
+        this.time = time
+    }
+
     constructor(latitude: Float, longitude: Float, time: Long) {
+        this.id = null
         this.latitude = latitude
         this.longitude = longitude
         this.time = time
     }
 
     constructor(latitude: Float, longitude: Float) {
+        this.id = null
         this.latitude = latitude
         this.longitude = longitude
         this.time = System.currentTimeMillis() / 1000
+    }
+
+    constructor() {
+        this.id = null
+        this.latitude = 0f
+        this.longitude = 0f
+        this.time = 0
     }
 }
 
@@ -49,9 +65,35 @@ class StorageSQL(context: Context, factory: SQLiteDatabase.CursorFactory?) :
         db.close()
     }
 
-    fun getAllEntries(): Cursor? {
-        val db = this.readableDatabase
-        return db.rawQuery("SELECT * FROM location", null)
+    fun getAllEntries(context: Context): ArrayList<Entry> {
+        val resultList = ArrayList<Entry>()
+        val dbHelper = StorageSQL(context, null)
+        val db = dbHelper.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM location ORDER BY _id DESC", null)
+        if (cursor.moveToFirst()) {
+            do {
+                val entry = Entry(cursor.getInt(cursor.getColumnIndex(COLUMN_ID)),
+                        cursor.getFloat(cursor.getColumnIndex("latitude")),
+                        cursor.getFloat(cursor.getColumnIndex("longitude")),
+                        cursor.getLong(cursor.getColumnIndex("datetime")))
+                resultList.add(entry)
+            } while (cursor.moveToNext())
+        }
+        return resultList
+    }
+
+    fun getEntryByID(context: Context, id: Int): Entry {
+        val entry = Entry()
+        val dbHelper = StorageSQL(context, null)
+        val db = dbHelper.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM location where " + COLUMN_ID + " = " + id, null)
+        if (cursor.moveToFirst()) {
+            entry.id = cursor.getInt(cursor.getColumnIndex("_id"))
+            entry.latitude = cursor.getFloat(cursor.getColumnIndex("latitude"))
+            entry.longitude = cursor.getFloat(cursor.getColumnIndex("longitude"))
+            entry.time = cursor.getLong(cursor.getColumnIndex("datetime"))
+        }
+        return entry
     }
 
     companion object {
